c723f286431c6e644ba44a6c0c15ab548ae53e31
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-16 13:33:14 -0500

    installed and setup strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb5868e35e0401bd6caead2808e9fee6'),
+    },
+  },
+});
