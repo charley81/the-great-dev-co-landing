de928b8009e52526550dccff05f7ee20f550f030
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-19 21:44:49 -0500

    added underline to title component

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-const H2Styles = styled('h2')`
+const TitleStyles = styled('div')`
   text-align: center;
   margin-bottom: 5rem;
+
+  .underline {
+    width: 10rem;
+    height: 0.25rem;
+    background: var(--primaryColor);
+    margin: 0.5rem auto 1rem;
+  }
 `
 
 const Title = ({ title, name }) => {
   return (
-    <>
-      <H2Styles className={name ? 'title' : ''}>{title || 'section'}</H2Styles>
-    </>
+    <TitleStyles>
+      <h2 className={name ? 'title' : ''}>{title || 'section'}</h2>
+      <div className="underline"></div>
+    </TitleStyles>
   )
 }
 
